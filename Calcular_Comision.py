4113b0a8d4d15f0170de89614a024598e03f0513
JavierDHG <madara098javier@gmail.com>
2022-08-03 12:31:49 -0500

    first commit

---
+++ Calcular_Comision.py
@@ -0,0 +1,5 @@
+nombre = input("Cual es su nombre: ")
+ventas = float(input("Cuanto has vendido este mes: "))
+comision = ventas * 13 / 100
+
+print(f"{nombre} ha ganado este mes {round(comision,2)}")
